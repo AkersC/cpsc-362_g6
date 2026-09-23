f3f932de1fadf6363d0ba9d6baef37fce565b122
Kevin Dao <daomkevin@gmail.com>
2026-09-23 13:04:22 -0700

    Modified movement to feel heavier

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,7 +5,10 @@ public class PlayerMovement : MonoBehaviour
 {
     [Header("Movement")]
     [SerializeField] private float moveSpeed = 5f;
+
+    [Header("Jumping")]
     [SerializeField] private float jumpForce = 7f;
+    [SerializeField] private float fallMultiplier = 2.5f;
 
     [Header("Ground Check")]
     [SerializeField] private Transform groundCheck;
@@ -22,17 +25,29 @@ public class PlayerMovement : MonoBehaviour
 
     private void FixedUpdate()
     {
+        // Left and right movement
         rb.linearVelocity = new Vector2(
             moveInput.x * moveSpeed,
             rb.linearVelocity.y
         );
+
+        // Makes the player fall faster
+        if (rb.linearVelocity.y < 0)
+        {
+            rb.linearVelocity += Vector2.up *
+                Physics2D.gravity.y *
+                (fallMultiplier - 1f) *
+                Time.fixedDeltaTime;
+        }
     }
 
+    // Called by the Move Input Action
     public void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
     }
 
+    // Called by the Jump Input Action
     public void OnJump(InputValue value)
     {
         if (value.isPressed && IsGrounded())
@@ -44,6 +59,7 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
+    // Checks if the player is touching the ground
     private bool IsGrounded()
     {
         return Physics2D.OverlapCircle(
